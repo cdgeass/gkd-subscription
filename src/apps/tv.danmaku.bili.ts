34a7c062a54dfdc58d84fe0be036b5bea90accd1
cdgeass <cdgeass97@gmail.com>
2024-09-22 18:45:42 +0800

    fix(app): fix tv.danmaku.bili snapshorturls

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -13,9 +13,9 @@ export default defineGkdApp({
       rules: [
         '[text="广告"] < * + * > @[text^="跳过"]',
         '[text="广告"] - * < * + * > @[text^="跳过"]',
-        'FrameLayout[childCount=2] +2 * > * + @[text^="跳过"]',
+        'TextView[text="立即查看"] - * < * +2 * > * + @[text^="跳过"]',
       ],
-      snapshotUrls: ['https://i.gkd.li/i/17083418'],
+      snapshotUrls: ['https://i.gkd.li/i/17083419'],
     },
   ],
 });
