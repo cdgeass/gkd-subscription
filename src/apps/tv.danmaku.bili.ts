d14600eb7ef6168514df3fabfc3ebb42a0a823a2
cdgeass <cdgeass97@gmail.com>
2024-09-27 20:11:01 +0800

    feat(app): update tv.danmaku.bili rule

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -10,17 +10,8 @@ export default defineGkdApp({
       fastQuery: true,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: [
-        '[text="广告"] < * + * > @[text^="跳过"]',
-        '[text="广告"] - * < * + * > @[text^="跳过"]',
-        '[text="广告"] - * < * -2 * > * + @[text^="跳过"]',
-        'TextView[text="立即查看"] - * < * +2 * > * + @[text^="跳过"]',
-        'TextView[text="进入活动页"] - * < * +2 * > * + @[text^="跳过"]',
-      ],
-      snapshotUrls: [
-        'https://i.gkd.li/i/17083419',
-        'https://i.gkd.li/i/17083441',
-      ],
+      rules: ['@[id="tv.danmaku.bili:id/count_down"][text^="跳过"]'],
+      snapshotUrls: ['https://i.gkd.li/i/17083419'],
     },
   ],
 });
