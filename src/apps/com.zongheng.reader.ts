f3d6e792dc02c2671cc106d07296ef93da2f8dcf
cdgeass <cdgeass97@gmail.com>
2025-01-08 17:15:45 +0800

    feat(app): update com.zongheng.reader

--- src/apps/com.zongheng.reader.ts
+++ src/apps/com.zongheng.reader.ts
@@ -11,18 +11,8 @@ export default defineGkdApp({
       matchRoot: true,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: ['TextView[text="立即查看"] < * - * > @[text^="跳过广告"]'],
-      snapshotUrls: ['https://i.gkd.li/i/17161250'],
-    },
-    {
-      key: 1,
-      name: '征集大赛',
-      fastQuery: true,
-      matchRoot: true,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: ['@ImageView[vid="tb"] - ImageView < RelativeLayout'],
-      snapshotUrls: ['https://i.gkd.li/i/17177780'],
-    },
+      rules: ['@[text^="跳过广告"] > [text~="[0-9]*"]'],
+      snapshotUrls: ['https://i.gkd.li/i/18344806'],
+    }
   ],
 });
